911bbda7db48180b2a7ab97f69a2dabee08e9b24
sanvikm <sanvikm@localhost>
2025-02-19 15:39:55 -0600

    my version

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,42 @@
 package studio4;
 
 import java.awt.Color;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.clear();
+		StdDraw.setXscale(0, 22);
+		StdDraw.setYscale(0, 16);
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.filledRectangle(0, 0, 22, 16);
+		StdDraw.setPenColor(255, 255, 255);
+		StdDraw.filledRectangle(11, 0, 2, 16);
+		StdDraw.filledRectangle(0, 8, 22, 2);
+		StdDraw.setPenColor(0, 0, 127);
+		StdDraw.filledRectangle(11, 0, 1, 16);
+		StdDraw.filledRectangle(0, 8, 22, 1);
+		StdDraw.setPenColor(255, 215, 0);
+		StdDraw.filledCircle(17.5, 13, 1.5);
+		StdDraw.filledCircle(4.5, 3, 1.5);
+		StdDraw.filledCircle(17.5, 3, 1.5);
+		StdDraw.filledCircle(4.5, 13, 1.5);
+		StdDraw.setPenColor(255, 255, 255);
+		StdDraw.setPenRadius(0.005);
+		StdDraw.circle(17.5, 13, 0.5);
+		StdDraw.circle(4.5, 3, 0.5);
+		StdDraw.circle(17.5, 3, 0.5);
+		StdDraw.circle(4.5, 13, 0.5);
+		
+		
+		
 	}
-}
+		
+	
+		
+	}
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -15,10 +15,26 @@ import edu.princeton.cs.introcs.StdDraw;
 public class InterpretDrawingFile {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		JFileChooser chooser = new JFileChooser("resources");
-		chooser.showOpenDialog(null);
-		File f = new File(chooser.getSelectedFile().getPath());
-		Scanner in = new Scanner(f); //making Scanner with a File
+		File f = new File("resources/rectangle_pink_filled.txt");
+		Scanner in = new Scanner(f); 
+		//JFileChooser chooser = new JFileChooser("resources");
+		//chooser.showOpenDialog(null);
+		//File f = new File(chooser.getSelectedFile().getPath());
+		//Scanner in = new Scanner(f); //making Scanner with a File
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isfilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+		
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		
+		StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+		
 		
 	}
 }
